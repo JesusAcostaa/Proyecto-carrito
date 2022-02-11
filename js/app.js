6b78c7d195e19bcc1a1c54deec72927346c73aa2
JesusAcostaArevalo <jesusacosta@wposs.com>
2022-02-11 16:51:01 -0500

    Agregar info a ventana carrito

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const carrito = document.querySelector("#carrito");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 const listaCursos = document.querySelector("#lista-cursos");
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -13,8 +14,36 @@ function cargarEventListeners() {
 function agregarCurso($event) {
   $event.preventDefault();
   if ($event.target.classList.contains("agregar-carrito")) {
-      console.log($event.target);
+    const cursoSeleccionado = $event.target.parentElement.parentElement;
+    leerDatosCurso(cursoSeleccionado);
   }
 }
 
+function leerDatosCurso(curso) {
+  const infoCurso = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
+  };
 
+  articulosCarrito = [...articulosCarrito, infoCurso];
+
+  console.log(articulosCarrito);
+
+  carritoHTML();
+}
+
+function carritoHTML() {
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            ${curso.titulo}
+        </td>
+        `;
+
+        contenedorCarrito.appendChild(row);
+    })
+}
